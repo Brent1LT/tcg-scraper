53e44afd9d9021dd7932367828fcf9acf0dced4f
Brent Bumann <brent.bumann@aol.com>
2023-01-01 23:41:04 -0800

    React Router working

--- src/components/splashPage.jsx
+++ src/components/splashPage.jsx
@@ -1,12 +1,14 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import '../App.css';
 
-function App() {
+const SplashPage = () => {
+
   return (
     <div className="App">
       <div>Welcome to the TCG Price Updator!</div>
       <div>Please enter the URL of the card you would like to track!</div>
     </div>
   );
-}
+};
 
-export default App;
+export default SplashPage;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SplashPage from './components/splashPage';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SplashPage />
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
